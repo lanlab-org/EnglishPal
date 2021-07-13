7f69d9a14d9bd1f12411a77369a080ecc8efe0fe
Hui Lan <lanhui@zjnu.edu.cn>
2021-07-13 12:46:31 +0800

    test scripts: change port from 5000 to 91.

--- app/test/test_login_security_fix.py
+++ app/test/test_login_security_fix.py
@@ -9,7 +9,7 @@ import random, string
 driver = webdriver.Remote('http://localhost:4444/wd/hub', DesiredCapabilities.CHROME)
 driver.implicitly_wait(10)
 
-HOME_PAGE = 'http://121.4.94.30:5000/'
+HOME_PAGE = 'http://121.4.94.30:91/'
 
 def test_login_security_fix():
     try:
